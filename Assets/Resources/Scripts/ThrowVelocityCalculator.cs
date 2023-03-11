3f603c0af63afeeb6faede936f055fca30f115b3
CEOFYEAST <89040723+CEOFYEAST@users.noreply.github.com>
2023-03-11 13:31:32 -0600

    added rack for the basketballs and fixed a ton of issues/refactored

--- Assets/Resources/Scripts/ThrowVelocityCalculator.cs
+++ Assets/Resources/Scripts/ThrowVelocityCalculator.cs
@@ -8,21 +8,23 @@ public class ThrowVelocityCalculator : MonoBehaviour
     //tweak the scale if shots are off
     public float scale = 4.5f;
 
-    public Vector3 goalPos;
-    public Vector3 hmdPos;
+    private Vector3 goalPos;
+    private Vector3 hmdPos;
 
-    public GameObject goalRing;
-    public GameObject mainCamera;
-    public GameObject basketball;
+    private GameObject goalRing;
+    private GameObject mainCamera;
+    //public GameObject basketball;
 
     public int updateIterations = 0;
 
     void Start()
     {
+        goalRing = GameObject.Find("Goal Ring");
+        mainCamera = GameObject.Find("Main Camera");
         goalPos = goalRing.transform.position;
     }
 
-    //algorith that sets the velocity of a shot proportional to the distance from the goal 
+    //algorithm that sets the velocity of a shot proportional to the distance from the goal 
     void Update()
     {
         if (updateIterations % 50 == 0)
@@ -31,7 +33,8 @@ public class ThrowVelocityCalculator : MonoBehaviour
             distance = (float)Math.Sqrt(Math.Pow(hmdPos.x, 2) + Math.Pow(hmdPos.z, 2));
             throwVelocity = distance * scale;
             //float velocityScale = distance/
-            basketball.GetComponent<UnityEngine.XR.Interaction.Toolkit.XRGrabInteractable>().throwVelocityScale = throwVelocity;
+            //basketball.GetComponent<UnityEngine.XR.Interaction.Toolkit.XRGrabInteractable>().throwVelocityScale = throwVelocity;
+            GetComponent<UnityEngine.XR.Interaction.Toolkit.XRGrabInteractable>().throwVelocityScale = throwVelocity;
             
         }
         updateIterations += 1;
